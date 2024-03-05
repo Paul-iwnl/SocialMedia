d0ddf7d1b02465ffd395e2df609ba45ea2ae2d49
Saswata Paul <saspaul2014@gmail.com>
2024-03-05 19:07:04 +0530

    done with the signup page

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import { Home } from "./_root/pages";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
 import './globals.css';
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
         {/* public route */}
-        <Route path="/sign-in" element={<SigninForm />} />
-        <Route path="/sign-up" element={<SignupForm />} />
+        <Route element={<AuthLayout />}>
+            <Route path="/sign-in" element={<SigninForm />} />
+            <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
         {/* private route */}
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+            <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   );
