ffd02e64dc35464dc8183f3a16bb69218e3fdf2e
Saswata Paul <saspaul2014@gmail.com>
2024-03-06 23:56:29 +0530

    added the ability to authenticate user during sign up and redirect them to home page

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,5 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {Form,FormControl,FormField,FormItem,FormLabel,FormMessage} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -7,10 +7,18 @@ import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/validation/index";
 import { z } from "zod";
 import Loader from "@/components/shared/Loader";
-import { createUserAccount } from "@/lib/appwrite/api";
+import { useToast } from "@/components/ui/use-toast";
+import {useCreateUserAccount, useSignInAccount} from '../../lib/react-query/queriesAndMutation'
+import { useUserContext } from "@/context/AuthContext";
 
 const SignupForm = () => {
-  const isLoading = false;
+  const { toast } = useToast();
+  const { checkAuthUser , isLoading:isUserLoading } = useUserContext();
+  const navigate = useNavigate();
+
+  const { mutateAsync: createUserAccount,isPending:isCreatingAccount } = useCreateUserAccount();
+
+  const { mutateAsync: signInAccount,isPending:isSigningIn} = useSignInAccount();
 
   // Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
@@ -27,7 +35,28 @@ const SignupForm = () => {
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
     const newUser = await createUserAccount(values);
 
-    console.log(newUser);
+    if(!newUser){
+      return toast({title: "Sign-up failed please try again."});
+    }
+
+    const session = await signInAccount({
+      email:values.email,
+      password:values.password
+    })
+
+    if(!session)
+    {
+      return toast({title: "Sign-in failed please try again."});
+    }
+
+    const isLoggedIn = await checkAuthUser();
+
+    if(isLoggedIn) {
+      form.reset();
+      navigate("/")
+    } else {
+      toast ({title: 'Sign up Failed Please try again. '})
+    }
   }
 
   return (
@@ -99,7 +128,7 @@ const SignupForm = () => {
           />
           <br />
           <Button type="submit" className="shad-button_primary">
-            {isLoading ? (
+            {isCreatingAccount ? (
               <div className="flex-center gap-2">
                 <Loader /> Loading...
               </div>
