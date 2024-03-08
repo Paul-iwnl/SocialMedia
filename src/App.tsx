5daed14c8d04c90d49202bf97accfa113f8d92a6
Saswata Paul <saspaul2014@gmail.com>
2024-03-08 22:22:38 +0530

    added all the route pages need for the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {Routes , Route} from "react-router-dom";
-import { Home } from "./_root/pages";
+import { Home,Explore,Saved,AllUsers,CreatePost,EditPost,PostDetails,Profile,UpdateProfile } from "./_root/pages";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
 import './globals.css';
@@ -13,12 +13,20 @@ const App = () => {
       <Routes>
         {/* public route */}
         <Route element={<AuthLayout />}>
-            <Route path="/sign-in" element={<SigninForm />} />
-            <Route path="/sign-up" element={<SignupForm />} />
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
         </Route>
         {/* private route */}
         <Route element={<RootLayout />}>
-            <Route index element={<Home />} />
+          <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
